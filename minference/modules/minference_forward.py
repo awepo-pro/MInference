5abbcbd3825ad4c294e6d51f05413f78e0f46e90
awepo <dc22762@um.edu.mo>
2026-01-16 00:52:15 +0800

    sync

--- minference/modules/minference_forward.py
+++ minference/modules/minference_forward.py
@@ -1479,6 +1479,8 @@ def minference_vllm_forward(
 
         assert query.shape[0] == num_prefill_query_tokens
         assert decode_query.shape[0] == num_decode_query_tokens
+
+        debug_print(attn_metadata)
         
         if prefill_meta := attn_metadata.prefill_metadata:
             # Prompt run.
