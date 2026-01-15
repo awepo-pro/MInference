0e8fbaad0cf7554fe6ea03557c66c58b448d0372
awepo <dc22762@um.edu.mo>
2026-01-15 20:54:21 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -121,7 +121,7 @@ def _triton_block_sparse_attn_fwd_kernel(
     # *     - Q might not contiguous tensor, stride is generalized method 
     # *     - blocks_ptr is contiguous, might use size of stride to compute
 
-    tl.device_print(f'{type(Q)=}')
+    tl.device_print(f'{Q.dtype=}')
     tl.device_print(H)
     tl.device_print(off_hz // H)
     tl.device_print(off_hz % H)
