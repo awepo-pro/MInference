b51811485a00c0f5c1c64cb934515a3e133f034c
awepo <dc22762@um.edu.mo>
2026-01-19 04:24:44 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -526,6 +526,7 @@ def _build_block_index_with_kvcache(
 
     key = get_full_key_from_cache(k_cache, block_tables, k_seqlen)
     debug_print(key.shape)
+    debug_print(query.shape)
 
     # * query.reshape := (b, n, seqlen, headdim) -> (b, n, seqlen // block_size_M, block_size_M, headdim)
     # * query.reshape.mean(dim=-2) := (b, n, seqlen // block_size_M, block_size_M, headdim) -> (b, n, seqlen // block_size_M, headdim)
@@ -545,7 +546,8 @@ def _build_block_index_with_kvcache(
 
     # * top_k cannot exceed p_pool[-1] dimension
     top_k = min(top_k, context_size // block_size_N)
-    
+
+    debug_print(context_size // block_size_N)
     debug_print(top_k)
     debug_print(p_pool.shape)
     
