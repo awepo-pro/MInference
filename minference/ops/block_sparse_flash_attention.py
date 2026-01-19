cd63ac826effc55da9c1c20f86834737e1a5d239
awepo <dc22762@um.edu.mo>
2026-01-20 06:28:47 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -377,7 +377,8 @@ def _triton_block_sparse_attn_fwd_kernel_with_kvcache(
         # tl.device_print('offs_d[:, None] * stride_k_headdim: ', offs_d[:, None] * stride_k_headdim)
         # tl.device_print('actual position: ', head_id * stride_num_khead + offs_d[:, None] * stride_k_headdim + physical_index * stride_kblock + (bt_block_index + offs_n)[None, :] * stride_kblock_size)
         # tl.device_print('actual k: ', k)
-        # idx = head_id * stride_num_khead + offs_d[:, None] * stride_k_headdim + physical_index * stride_kblock + (bt_block_index + offs_n)[None, :] * stride_kblock_size
+        idx = head_id * stride_num_khead + offs_d[:, None] * stride_k_headdim + physical_index * stride_kblock + (bt_block_index + offs_n)[None, :] * stride_kblock_size
+        tl.device_print('idx: ', idx)
         tl.device_print('actual k[0], ', k)
     
         v = tl.load(v_ptrs)
