fab6151d54de4daf21317ac906a4831f5256e1b2
awepo <dc22762@um.edu.mo>
2026-01-20 03:15:30 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -314,7 +314,7 @@ def _triton_block_sparse_attn_fwd_kernel_with_kvcache(
     # * NUM_ROWS := no. of rows in each block
     # * off_hz * NUM_ROWS := move to the current head; start_n := determine current block
     # blocks_ptr = block_index + (off_hz * NUM_ROWS + start_n) * MAX_BLOCKS_PRE_ROW
-    tl.device_print('old blocks ptr: ', block_index + (off_hz * NUM_ROWS + start_n) * MAX_BLOCKS_PRE_ROW)
+    tl.device_print('old blocks ptr: ', (off_hz * NUM_ROWS + start_n) * MAX_BLOCKS_PRE_ROW)
     blocks_ptr = block_index + (start_n * NUM_ROWS) * MAX_BLOCKS_PRE_ROW
     tl.device_print('new blocks ptr: ', (start_n * NUM_ROWS) * MAX_BLOCKS_PRE_ROW)
 
