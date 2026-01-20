295464c5e9788c5ef5c8918a3628af2c241d662b
awepo <dc22762@um.edu.mo>
2026-01-20 17:03:32 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -179,7 +179,8 @@ def _triton_block_sparse_attn_fwd_kernel(
         l_i = l_i * alpha + tl.sum(p, 1)
         m_i = m_i_new
 
-        offset_acc = tl.arange(0, BLOCK_M)[None, :] + tl.arange(0, BLOCK_DMODEL)[:, None]
+        # * (BLOCK_M, 1) \plus (1, BLOCK_DMODEL)
+        offset_acc = tl.arange(0, BLOCK_M)[:, None] + (tl.arange(0, BLOCK_DMODEL) * BLOCK_M)[None, :]
         tl.device_print('acc: ', acc + offset_acc)
         # tl.device_print('li: ', l_i + tl.arange(0, BLOCK_M))
         # tl.device_print('mi: ', m_i + tl.arange(0, BLOCK_M))
@@ -436,9 +437,11 @@ def _triton_block_sparse_attn_fwd_kernel_with_kvcache(
         l_i = l_i * alpha + tl.sum(p, 1)
         m_i = m_i_new
 
-        # tl.device_print('acc: ', acc)
-        tl.device_print('li: ', l_i + tl.arange(0, BLOCK_M))
-        tl.device_print('mi: ', m_i + tl.arange(0, BLOCK_M))
+        # * (BLOCK_M, 1) \plus (1, BLOCK_DMODEL)
+        offset_acc = tl.arange(0, BLOCK_M)[:, None] + (tl.arange(0, BLOCK_DMODEL) * BLOCK_M)[None, :]
+        tl.device_print('acc: ', acc + offset_acc)
+        # tl.device_print('li: ', l_i + tl.arange(0, BLOCK_M))
+        # tl.device_print('mi: ', m_i + tl.arange(0, BLOCK_M))
 
     # write back O
     acc /= l_i[:, None]
