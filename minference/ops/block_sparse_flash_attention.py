ded6ae5926cada0641f1c9d0798c5faabce4cd9a
awepo <dc22762@um.edu.mo>
2026-01-21 04:09:31 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -610,7 +610,7 @@ def get_full_key_from_cache(k_cache, block_tables, seqlen):
     # block_tables: (#batch, max_block_per_seq)
     # We take only the first num_blocks_needed blocks
     assert block_tables.shape[0] == 1, f'{block_tables.shape=}, where [0] != 1'
-    block_indices = block_tables[:, :num_blocks_needed]  # (#batch, num_blocks_needed)
+    block_indices = block_tables[:, :num_blocks_needed]  # * block_indices \in (#batch, num_blocks_needed)
     
     # Gather the blocks from k_cache
     # k_cache[block_indices] would give us (#batch, num_blocks_needed, block_size, #kv_head, headdim)
@@ -620,7 +620,10 @@ def get_full_key_from_cache(k_cache, block_tables, seqlen):
     # (#batch, num_blocks_needed * block_size, #kv_head, headdim)
     po_debug.debug_print(gathered_blocks.shape)
     po_debug.debug_print(k_cache.shape)
-
+    po_debug.debug_print(num_blocks_needed)
+    po_debug.debug_print(block_size)
+    po_debug.debug_print(num_kv_heads)
+    po_debug.debug_print(num_blocks_needed * block_size)
     full_key = gathered_blocks.reshape(batch_size, num_blocks_needed * block_size, num_kv_heads, head_dim)
     
     # Trim to actual sequence length
