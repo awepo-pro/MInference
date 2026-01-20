db91364c43694529c28878008fc6332d2e125d57
awepo <dc22762@um.edu.mo>
2026-01-21 00:30:15 +0800

    sync

--- minference/modules/minference_forward.py
+++ minference/modules/minference_forward.py
@@ -1431,6 +1431,8 @@ def minference_vllm_forward(
 
                 # * cannot use output[:, head, :] since it immediately squeeze out the middle dimension
                 output[:, head:head+1, :] = out
+
+                break
                 
                 # exit()
             return output
