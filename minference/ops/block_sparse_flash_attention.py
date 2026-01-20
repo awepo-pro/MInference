dfd856e87038fcd56464ce95d2507741451c571b
awepo <dc22762@um.edu.mo>
2026-01-20 14:24:45 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -263,7 +263,7 @@ def _triton_block_sparse_attn_fwd_kernel_with_kvcache(
     # * b \times h
     off_hz = tl.program_id(1)
 
-    k_tmp = tl.load(k_cache + tl.arange(0, 10))
+    k_tmp = tl.load(k_cache + tl.arange(0, 10) * stride_qk)
     tl.device_print('k-tmp: ', k_tmp)
 
     # assert off_hz == 0, f'{off_hz=} != 0'
