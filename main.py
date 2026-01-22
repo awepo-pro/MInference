2cebcfbb8484fa153125e3e565535f230dd8c165
awepo <dc22762@um.edu.mo>
2026-01-23 03:32:06 +0800

    sync

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ def main() -> None:
         enforce_eager=True,     # disable to get 2-3x faster speed for CUDA graph
         dtype='float16',
         max_model_len=12800,
-        # block_size=256,
-        block_size=16,
+        block_size=256,
+        # block_size=16,
         enable_chunked_prefill=True,
         # max_num_batched_tokens=64
         max_num_batched_tokens=16
