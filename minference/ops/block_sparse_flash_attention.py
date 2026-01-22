81fb226213198f94dd726c4681878c88ec1c3d31
awepo <dc22762@um.edu.mo>
2026-01-23 06:07:58 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -694,6 +694,8 @@ def block_sparse_attention(
     assert num_heads == 1, f'{num_heads=} != 1'
     
     pad = block_size_M - (query.shape[2] & (block_size_M - 1))
+    po_debug.debug_print(query.shape[2])
+    po_debug.debug_print(query.shape[2] + pad)
     query = torch.nn.functional.pad(query, [0, 0, 0, pad, 0, 0, 0, 0])
     # po_debug.debug_print(query.stride())
     # exit()
