d21dc4389788f8f158dfb124c86a9ad06186550c
awepo <dc22762@um.edu.mo>
2026-01-24 04:52:12 +0800

    sync

--- main.py
+++ main.py
@@ -88,9 +88,9 @@ def main() -> None:
         max_model_len=12800,
         block_size=256,
         # block_size=16,
-        enable_chunked_prefill=True,
+        # enable_chunked_prefill=True,
         # max_num_batched_tokens=64
-        max_num_batched_tokens=16
+        # max_num_batched_tokens=16
     )
 
     # Patch MInference Module
