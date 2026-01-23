cb07fdfd192a6d9d2c9af235c52bb67c83f4ed28
awepo <dc22762@um.edu.mo>
2026-01-23 17:59:52 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -513,7 +513,7 @@ def _triton_block_sparse_attn_fwd_kernel_with_kvcache(
         #         + (bt_block_index + offs_n)[None, :] * stride_kblock_size
 
         k_ptrs = k_base_ptrs \
-                + (physical_index + bt_block_index / BLOCK_SIZE).to(tl.int32) * stride_kblock \
+                + ((physical_index + bt_block_index / BLOCK_SIZE) * stride_kblock).to(tl.int32) \
                 + offs_n[None, :] * stride_kblock_size
 
         
