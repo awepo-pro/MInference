a7f6cb226bab4ab6cf66a79037be67ad7ae33746
awepo <dc22762@um.edu.mo>
2026-01-27 01:21:28 +0800

    sync

--- minference/ops/block_sparse_flash_attention.py
+++ minference/ops/block_sparse_flash_attention.py
@@ -38,6 +38,9 @@ def _build_block_index(
 
     # * (b, n, q_seqlen // block_size_M, k_seqlen // block_size_N)
     p_pool = torch.einsum('bhmk, bhnk -> bhmn', query_pool, key_pool)
+
+    po_debug.debug_print(p_pool)
+    
     # po_debug.debug_print(key_pool[0, 0, 0, :])
     # * build 4D, arrange_M \in (b=1, h=1, m, 1); arange_N \in (b=1, h=1, 1, n). build a mask in last 2 dimension. should be a upper-triangular matrix
     p_pool = p_pool.where(arange_M[None, None, :, None] >= arange_N[None, None, None, :], -torch.inf)
@@ -866,6 +869,8 @@ def _build_block_index_with_kvcache(
     p_pool = p_pool.where(arange_M[None, None, :, None] + abs_query_pos >= arange_N[None, None, None, :], -torch.inf)
     # po_debug.debug_print(arange_M[None, None, :, None] + abs_query_pos >= arange_N[None, None, None, :])
     # po_debug.debug_print(arange_M[None, None, :, None] >= arange_N[None, None, None, :])
+
+    po_debug.debug_print(p_pool)
 
     # * top_k cannot exceed p_pool[-1] dimension, 
     # * assert ceil_div(k_seqlen, block_size_N) == k_seqlen_pad // block_size_N, since k_seqlen_pad := ceil_div(k_seqlen, block_size_N) * block_size_N
